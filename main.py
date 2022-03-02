a1e08a41c493c3000fe25fdf8b46c5f7e3534627
ivankossdev <ivan.koss@yandex.ru>
2022-03-02 13:59:21 +0300

    ListView

---
+++ main.py
@@ -0,0 +1,50 @@
+import sys
+from des import *
+# pyuic5 des.ui -o des.py
+# pyinstaller -F -w main.py
+# pyrcc5 res.qrc -o des.py
+# pyinstaller -F -w -i BD.ico main.py
+
+class App(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(App, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.listWidget.insertItem(0, 'Red')
+        self.ui.listWidget.insertItem(1, 'Orange')
+        self.ui.listWidget.clicked.connect(self.click_widget_1)
+        self.ui.listWidget_2.clicked.connect(self.click_widget_2)
+        self.ui.pushButton.clicked.connect(self.send_button)
+        self.ui.pushButton_2.clicked.connect(self.resend_button)
+        self.selected = None
+
+    def click_widget_1(self):
+        self.selected = self.ui.listWidget.currentRow()
+        print(self.selected)
+
+    def click_widget_2(self):
+        self.selected = self.ui.listWidget_2.currentRow()
+        print(self.selected)
+
+    def send_button(self):
+        if self.selected is not None:
+            self.ui.listWidget_2.insertItem(self.selected,
+                                            self.ui.listWidget.takeItem(self.selected).text())
+            self.selected = None
+        else:
+            pass
+
+    def resend_button(self):
+        if self.selected is not None:
+            self.ui.listWidget.insertItem(self.selected,
+                                            self.ui.listWidget_2.takeItem(self.selected).text())
+            self.selected = None
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = App()
+    myapp.show()
+    sys.exit(app.exec_())
